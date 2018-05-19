a8c216b9aad8859804c013073209347f199941d1
Rajesh Babu <sammydave.e@gmail.com>
2018-05-19 23:09:10 +0200

    going to heroku now

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return <div>Hello React! My love</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
